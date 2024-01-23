5cd49efd8049604d0ac9058ae7961065df633188
Romeo Lindström <123453592+romeomlindstrom@users.noreply.github.com>
2024-01-23 12:44:37 +0200

    Changed anchor tag to span for Bootstrap cards

--- js/index.js
+++ js/index.js
@@ -1,17 +1,22 @@
 let inventoryItems;
+let orderItems;
+
+function addItemToOrder(itemID) {
+    console.log(itemID);
+}
 
 function printInventory(categoryID) {
     let inventoryContainer = document.getElementById("inventoryContainer");
     let htmlContent = "";
     for (let i = 0; i < inventoryItems.length; i++) {
         if (inventoryItems[i].items_category === categoryID) {
-            htmlContent += '<div class="card mt-2"> <div class="card-body"> <h5 class="card-title">' + inventoryItems[i].items_name + '</h5>';
+            htmlContent += '<div class="card mt-2"> <span onclick="addItemToOrder(' + inventoryItems[i].items_ID + ')"> <div class="card-body"> <h5 class="card-title">' + inventoryItems[i].items_name + '</h5>';
             
             if (inventoryItems[i].items_description !== null) {
                 htmlContent += '<p class="card-text">' + inventoryItems[i].items_description + '</p>';
             }
 
-            htmlContent += '</div> </div>';
+            htmlContent += '</div> </span> </div>';
         }
     }
     inventoryContainer.innerHTML = htmlContent;
